d16b165500aedd443401b32ee7ac8e8affe3d22b
pinapp <49245367+pinapp@users.noreply.github.com>
2024-01-02 17:32:56 +0800

    CHEN

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(function() {
-	const html = document.body.childNodes[0].innerHTML
+	let html = document.body.childNodes[0].innerHTML
 	
 	function isJSON(str) {
 		try {
@@ -15,40 +15,47 @@ $(function() {
 		}
 	}
 	
-	// 说明是json字符串
-	if(isJSON(html)) {
-		const obj = JSON.parse(html)
-		
-		window.IsCollapsible = true  // 显示控制
-		window.TAB = "    "
-		window.ImgExpanded = true
-		window.SINGLE_TAB = "  ";
-		window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
-		window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
-		
-		const content = ProcessObject(obj, 0, false, false, false);
-		document.body.childNodes[0].innerHTML = content
-		
-		var script =  document.createElement("script")
-		const appjs = `
-		window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
-		window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
-		
-		function ExpImgClicked(img){
-		  var container = img.parentNode.nextSibling;
-		  if(!container) return;
-		  var disp = "none";
-		  var src = window.ImgCollapsed;
-		  if(container.style.display == "none"){
-		      disp = "inline";
-		      src = window.ImgExpanded;
-		  }
-		
-		  container.style.display = disp;
-		  img.src = src;
+	if(!html) {
+		html = document.body.childNodes[0].textContent
+	}
+	
+	if(html) {
+		// 说明是json字符串
+		if(isJSON(html)) {
+			const obj = JSON.parse(html)
+			
+			window.IsCollapsible = true  // 显示控制
+			window.TAB = "    "
+			window.ImgExpanded = true
+			window.SINGLE_TAB = "  ";
+			window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
+			window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
+			
+			const content = ProcessObject(obj, 0, false, false, false);
+			
+			document.body.innerHTML = `<pre>${content}</pre>`
+			
+			var script =  document.createElement("script")
+			const appjs = `
+			window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
+			window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
+			
+			function ExpImgClicked(img){
+			  var container = img.parentNode.nextSibling;
+			  if(!container) return;
+			  var disp = "none";
+			  var src = window.ImgCollapsed;
+			  if(container.style.display == "none"){
+				  disp = "inline";
+				  src = window.ImgExpanded;
+			  }
+			
+			  container.style.display = disp;
+			  img.src = src;
+			}
+			`
+			script.innerHTML = appjs
+			document.body.appendChild(script)
 		}
-		`
-		script.innerHTML = appjs
-		document.body.appendChild(script)
 	}
 })
