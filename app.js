5ea09ef208b8ddec282314208823b02f52af31d2
pinapp <49245367+pinapp@users.noreply.github.com>
2024-01-02 17:45:26 +0800

    CHEN

--- app.js
+++ app.js
@@ -22,40 +22,43 @@ $(function() {
 	if(html) {
 		// 说明是json字符串
 		if(isJSON(html)) {
-			const obj = JSON.parse(html)
-			
-			window.IsCollapsible = true  // 显示控制
-			window.TAB = "    "
-			window.ImgExpanded = true
-			window.SINGLE_TAB = "  ";
-			window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
-			window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
-			
-			const content = ProcessObject(obj, 0, false, false, false);
-			
-			document.body.innerHTML = `<pre>${content}</pre>`
-			
-			var script =  document.createElement("script")
-			const appjs = `
-			window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
-			window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
-			
-			function ExpImgClicked(img){
-			  var container = img.parentNode.nextSibling;
-			  if(!container) return;
-			  var disp = "none";
-			  var src = window.ImgCollapsed;
-			  if(container.style.display == "none"){
-				  disp = "inline";
-				  src = window.ImgExpanded;
-			  }
-			
-			  container.style.display = disp;
-			  img.src = src;
+			try {
+				const obj = JSON.parse(html)
+				
+				window.IsCollapsible = true  // 显示控制
+				window.TAB = "    "
+				window.ImgExpanded = true
+				window.SINGLE_TAB = "  ";
+				window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
+				window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
+				
+				const content = ProcessObject(obj, 0, false, false, false);
+				
+				document.body.innerHTML = `<pre>${content}</pre>`
+				
+				var script =  document.createElement("script")
+				const appjs = `
+				window.ImgCollapsed = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Collapsed.gif";
+				window.ImgExpanded = "https://webhelper.oss-cn-hangzhou.aliyuncs.com/Expanded.gif";
+				
+				function ExpImgClicked(img){
+				  var container = img.parentNode.nextSibling;
+				  if(!container) return;
+				  var disp = "none";
+				  var src = window.ImgCollapsed;
+				  if(container.style.display == "none"){
+					  disp = "inline";
+					  src = window.ImgExpanded;
+				  }
+				
+				  container.style.display = disp;
+				  img.src = src;
+				}
+				`
+				script.innerHTML = appjs
+				document.body.appendChild(script)
 			}
-			`
-			script.innerHTML = appjs
-			document.body.appendChild(script)
+			catch(e) {}
 		}
 	}
 })
